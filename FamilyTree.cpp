780d1853bda4f9a10a50ca0f7466275d28d45233
Eldar Takach <eldartakach@gmail.com>
2020-04-02 15:03:42 +0300

    started implementation

---
+++ FamilyTree.cpp
@@ -0,0 +1,38 @@
+#include "FamilyTree.hpp"
+#include <iostream>
+#include <string>
+using namespace family;
+Tree::Tree(std::string name){
+    
+}
+
+
+Tree& Tree::T(std::string root){
+    
+} 
+   
+Tree& Tree::addFather(std::string son, std::string dad){
+
+
+}
+Tree& Tree::addMother(std::string son, std::string mom){
+
+
+}
+
+std::string Tree::relation(std::string name){
+
+
+}
+
+std::string Tree::find(std::string relation){
+
+}
+
+void Tree::display(){
+
+}
+
+void Tree::remove(std::string name){
+    
+}
