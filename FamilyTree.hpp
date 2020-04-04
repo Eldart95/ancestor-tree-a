dd5cd22c8a0f5c9b3a739246fd94cba8876c5b30
Eldar Takach <eldartakach@gmail.com>
2020-04-04 16:37:27 +0300

    new headers

--- FamilyTree.hpp
+++ FamilyTree.hpp
@@ -2,27 +2,37 @@
 #include <iostream>
 #include <string>
 namespace family{
-class Tree{
-    private:
-    Node root;
-    Node father;
-    Node mother;
-    public:
-    Tree(std::string root);
-    Tree& T(std::string person);
-    Tree& addFather(std::string son ,std::string dad);
-    Tree& addMother(std::string son, std::string mom);
-    std::string relation(std::string name);
-    std::string find(std::string relation);
-    void display();
-    void remove(std::string name);
+    class Tree{
+        
+        
+        struct Node* root;
+        public:
+        Tree();
+        ~Tree();
+        Tree(std::string root);
+        Tree& T(std::string person);
+        Tree& addFather(std::string son ,std::string dad);
+        Tree& addMother(std::string son, std::string mom);
+        std::string relation(std::string name);
+        std::string find(std::string relation);
+        void display();
+        void remove(std::string name);
+        private:
+        void addF(Node* temp,std::string son,std::string dad);
+        void addM(Node* t,std::string son,std::string mom);
+        void disp(Node* t,int n);
+        std::string relation_locate(Node* t,std::string name);
 
 };
-class Node{
-    private:
-    std::string name;
-    public:
-    Node(std::string person);
+    struct Node{
+            public:
+            std::string name;
+            struct Node* mother;
+            struct Node* father;
+            struct Node* son;
+            public:
+            Node(std::string name);
     
-};
+        };
+
 }
