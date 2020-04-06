12556b43ca5846adb69c95fb7e718203098c0b29
Eldar Takach <eldartakach@gmail.com>
2020-04-06 14:31:45 +0300

    new function's headers

--- FamilyTree.hpp
+++ FamilyTree.hpp
@@ -18,10 +18,13 @@ namespace family{
         void display();
         void remove(std::string name);
         private:
-        void addF(Node* temp,std::string son,std::string dad);
-        void addM(Node* t,std::string son,std::string mom);
+        void addF(Node* temp,std::string son,std::string dad,int count);
+        void addM(Node* t,std::string son,std::string mom,int count);
         void disp(Node* t,int n);
+        std::string name_locate(Node* t,std::string relation);
         std::string relation_locate(Node* t,std::string name);
+        void remove_sub(Node* t,std::string name);
+        void freeALL(Node* temp);
 
 };
     struct Node{
@@ -30,6 +33,7 @@ namespace family{
             struct Node* mother;
             struct Node* father;
             struct Node* son;
+            std::string relation;
             public:
             Node(std::string name);
     
